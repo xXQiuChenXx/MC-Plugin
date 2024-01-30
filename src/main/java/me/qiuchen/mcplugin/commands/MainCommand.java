cd77050d26ff51d7c7ea64a8da63127be167403c
xXQiuChenXx <lautaihong2004@gmail.com>
2024-01-30 13:53:16 +0800

    target player

--- src/main/java/me/qiuchen/mcplugin/commands/MainCommand.java
+++ src/main/java/me/qiuchen/mcplugin/commands/MainCommand.java
@@ -1,26 +1,42 @@
 package me.qiuchen.mcplugin.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import me.qiuchen.mcplugin.Main;
 
+import java.util.Objects;
+
 public class MainCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if(!command.getName().equalsIgnoreCase("hi")) return true;
-
-        if(sender instanceof Player player) {
+        if (sender instanceof Player player) {
             player.sendMessage("Hello, Welcome to plugin dev!");
 
-        } else if(sender instanceof ConsoleCommandSender) {
+            if (strings.length == 0) return true;
+
+            if (strings[0].equals(("reload")) && sender.hasPermission("mc.reload")) {
+                Main.reload();
+            } else {
+                player.sendMessage("You dont have the permission to use this command.");
+            }
+
+            if (strings[0].equals("heal")) {
+                Player targetPlayer = Bukkit.getServer().getPlayerExact(strings[1]);
+
+                Objects.requireNonNullElse(targetPlayer, player).setHealth(10);
+                Objects.requireNonNullElse(targetPlayer, player).setFoodLevel(10);
+            }
+
+
+        } else if (sender instanceof ConsoleCommandSender) {
             System.out.println("Hello Console!");
 
-        } else if(sender instanceof BlockCommandSender) {
+        } else if (sender instanceof BlockCommandSender) {
             System.out.println("The command was run by a command block.");
         }
 
-        if(strings.length > 0 && strings[0].equals(("reload"))) Main.reload();
 
         return true;
     }
--- src/main/java/me/qiuchen/mcplugin/listeners/MyEvents.java
+++ src/main/java/me/qiuchen/mcplugin/listeners/MyEvents.java
@@ -33,15 +33,9 @@ public class MyEvents implements Listener {
         Player player = event.getPlayer();
         TextComponent component = Component.text("Welcome to the server @" + player.getName());
         event.joinMessage(component);
-        System.out.println(player.getName());
 
         if(player.getName().equalsIgnoreCase("YuanXeow")) {
             System.out.println("Test PASS");
-            try {
-                Thread.sleep(5000);
-            } catch(InterruptedException e) {
-                //
-            }
             player.setGameMode(GameMode.CREATIVE);
         }
     }
