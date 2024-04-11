d8c1f79266ac387afac112e10a683b3f86b7b754
xXQiuChenXx <lautaihong2004@gmail.com>
2024-04-11 22:34:36 +0800

    Remove unecessary

--- src/main/java/me/qiuchen/mcplugin/commands/MainCommandTabComplete.java
+++ src/main/java/me/qiuchen/mcplugin/commands/MainCommandTabComplete.java
@@ -1,4 +1,56 @@
 package me.qiuchen.mcplugin.commands;
 
-public class MainCommandTabComplete {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MainCommandTabComplete implements TabCompleter {
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (!(commandSender instanceof Player)) {
+            return null;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        Player player = (Player) commandSender;
+        if (args.length == 1) {
+            if (player.hasPermission("debugstickpro.help")) {
+                list.add("help");
+            }
+            if (player.hasPermission("debugstickpro.reload")) {
+                list.add("reload");
+            }
+            if (player.hasPermission("debugstickpro.give")) {
+                list.add("give");
+            }
+            if (player.hasPermission("debugstickpro.mode")) {
+                list.add("mode");
+            }
+            return list;
+        }
+
+        if (args.length == 2) {
+
+            if ("give".equalsIgnoreCase(args[0])) {
+                return null;
+            }
+
+            if (args[0].equalsIgnoreCase("mode")) {
+                if (player.hasPermission("debugstickpro.mode")) {
+                    list.add("classic");
+                }
+                if (player.hasPermission("debugstickpro.mode.copy")) {
+                    list.add("copy");
+                }
+                if (player.hasPermission("debugstickpro.mode.freeze")) {
+                    list.add("freeze");
+                }
+                return list;
+            }
+        }
+        return list;
+    }
 }
