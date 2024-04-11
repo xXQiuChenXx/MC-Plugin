bd0055d97a73eee6e6c7298b1d407621d1097310
xXQiuChenXx <lautaihong2004@gmail.com>
2024-04-11 22:37:47 +0800

    updates

--- src/main/java/me/qiuchen/mcplugin/commands/MainCommandTabComplete.java
+++ src/main/java/me/qiuchen/mcplugin/commands/MainCommandTabComplete.java
@@ -3,32 +3,32 @@ package me.qiuchen.mcplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainCommandTabComplete implements TabCompleter {
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (!(commandSender instanceof Player)) {
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (!(commandSender instanceof Player player)) {
             return null;
         }
         ArrayList<String> list = new ArrayList<>();
-        Player player = (Player) commandSender;
         if (args.length == 1) {
-            if (player.hasPermission("debugstickpro.help")) {
-                list.add("help");
-            }
-            if (player.hasPermission("debugstickpro.reload")) {
+            if (player.hasPermission("mcp.reload")) {
                 list.add("reload");
             }
-            if (player.hasPermission("debugstickpro.give")) {
-                list.add("give");
+            if (player.hasPermission("mcp.fly")) {
+                list.add("fly");
             }
-            if (player.hasPermission("debugstickpro.mode")) {
-                list.add("mode");
+            if (player.hasPermission("mcp.heal")) {
+                list.add("heal");
             }
+            list.add("gui");
+
             return list;
         }
 
