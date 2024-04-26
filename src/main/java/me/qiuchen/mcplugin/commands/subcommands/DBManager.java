f2da5e7b1376e99dd017a6253c907f2121b8ec13
xXQiuChenXx <lautaihong2004@gmail.com>
2024-04-26 23:31:13 +0800

    database

---
+++ src/main/java/me/qiuchen/mcplugin/commands/subcommands/DBManager.java
@@ -0,0 +1,9 @@
+package me.qiuchen.mcplugin.commands.subcommands;
+
+public class DBManager {
+    public static void Main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+};
+
